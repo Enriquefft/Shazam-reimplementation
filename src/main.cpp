3d8a2cd5e3cc1e470447a0bbde3cacc7147e5d43
Enriquefft <enriquefft2001@gmail.com>
2024-07-10 00:20:45 -0500

    feat: added song indexing secondary memory retrieval

--- src/main.cpp
+++ src/main.cpp
@@ -1,101 +1,78 @@
 #include "AudioFile.hpp"
-#include "Spectrogram.hpp"
-#include <chrono>
-#include <csvdumps.hpp>
+#include "UnorderedMap.hpp"
+#include <algorithm>
 #include <filesystem>
-#include <iostream>
 
-#include <SongIndexing.hpp>
+#include <spdlog/spdlog.h>
+#include <spdlog/stopwatch.h>
 
-using std::cout;
-namespace fs = std::filesystem;
 using TypeParam = double;
+
+using hash_t = std::pair<uint32_t, size_t>;
+
+constexpr auto count_matches(const std::vector<hash_t> &song_hash,
+                             const std::vector<hash_t> &hashes) -> size_t {
+  size_t match_count = 0;
+
+  std::ranges::for_each(song_hash, [&](const auto &hash_frame) {
+    std::ranges::for_each(hashes, [&](const auto &hash) {
+      if (hash_frame == hash) {
+        match_count++;
+      }
+    });
+  });
+
+  return match_count;
+}
 
 auto main() -> int {
 
-  hash_songs<TypeParam>("./assets", "./experiments/hashes");
-  /*
-  std::vector<std::filesystem::path> songs = {
-      "assets/the_bidding.wav", "assets/the_bidding_noisy_sample.wav"};
+  spdlog::set_level(spdlog::level::debug);
+  spdlog::stopwatch sw_total; // Total time stopwatch
 
+  const std::string HASHES_FILE = "songs_map.bin";
+  const std::filesystem::path SONG_TO_SEARCH = "assets/banana_man_sample.wav";
 
-  for (const auto &song : songs) {
+  std::cout << "Searching for " << SONG_TO_SEARCH << " in the database\n";
 
-    cout << "Reading audio..." << '\n';
-    cout << "Hashing " << song.string() << '\n';
+  spdlog::stopwatch sw_step1;
+  auto songs_map = read_songs_map_from_file<TypeParam>(HASHES_FILE);
+  spdlog::debug("Step 1 (Read songs_map) took {} seconds",
+                sw_step1.elapsed().count());
 
-    auto start = std::chrono::high_resolution_clock::now();
-    Audio<TypeParam> audio(song);
-    auto end = std::chrono::high_resolution_clock::now();
-    auto m_seconds = std::chrono::duration_cast<std::chrono::milliseconds>(
-                         std::chrono::high_resolution_clock::now() - start)
-                         .count();
+  spdlog::stopwatch sw_step2;
+  Audio<TypeParam> song(SONG_TO_SEARCH);
+  spdlog::debug("Step 2 generate song audiodata took {} seconds",
+                sw_step2.elapsed().count());
 
-    cout << "Read audio in " << m_seconds << " ms" << '\n';
+  spdlog::stopwatch sw_step3;
+  Spectrogram<TypeParam> spectrogram(song);
+  spdlog::debug("Step 3 generate song spectrogram took {} seconds",
+                sw_step3.elapsed().count());
 
-    cout << "Building Spectrogram..." << '\n';
+  spdlog::stopwatch sw_step4;
+  spectrogram.get_local_maximums();
+  auto song_hash = spectrogram.get_hashes();
+  spdlog::debug("Step 4 generate song hashes took {} seconds",
+                sw_step4.elapsed().count());
 
-    start = std::chrono::high_resolution_clock::now();
-    Spectrogram<TypeParam> spectrogram(audio);
-    end = std::chrono::high_resolution_clock::now();
-    m_seconds = std::chrono::duration_cast<std::chrono::milliseconds>(
-                    std::chrono::high_resolution_clock::now() - start)
-                    .count();
+  std::unordered_map<std::string, size_t> matches;
 
-    cout << "Built spectrogram in " << m_seconds << " ms" << '\n';
+  spdlog::stopwatch sw_step5;
+  for (const auto &[song_name, song_hashes] : songs_map) {
+    matches[song_name] = count_matches(song_hash, song_hashes);
+  }
+  spdlog::debug(
+      "Step 5 compare hash to every song in songs_map took {} seconds",
+      sw_step5.elapsed().count());
 
-    cout << "Extracting features..." << '\n';
-    start = std::chrono::high_resolution_clock::now();
-    auto local_max = spectrogram.get_local_maximums();
-    end = std::chrono::high_resolution_clock::now();
-    m_seconds = std::chrono::duration_cast<std::chrono::milliseconds>(
-                    std::chrono::high_resolution_clock::now() - start)
-                    .count();
-
-    cout << "Found " << local_max.size() << " features in " << m_seconds
-         << " ms" << '\n';
-
-    cout << "Computing hashes..." << '\n';
-
-    start = std::chrono::high_resolution_clock::now();
-    auto hashes = spectrogram.get_hashes();
-    end = std::chrono::high_resolution_clock::now();
-    m_seconds = std::chrono::duration_cast<std::chrono::milliseconds>(
-                    std::chrono::high_resolution_clock::now() - start)
-                    .count();
-    cout << "Computed " << hashes.size() << " hashes in " << m_seconds << " ms"
-         << '\n';
-
-    cout << "Dumping hashes..." << '\n';
-    auto hashpath = song.parent_path().parent_path() / "experiments" /
-                    (song.stem().string() + "_hashes.csv");
-    csv_write_hashes(hashes, hashpath);
-    cout << "Hashes dumped at " << hashpath.string() << '\n';
-
-    // auto complex_spectrogram = Spectrogram<TypeParam>::stft(audio);
-
-    // std::complex<TypeParam> comp_sum = sum_vector(complex_spectrogram);
-
-    // print("complex_spectrogram_sum: {}, {}", comp_sum.real(),
-    // comp_sum.imag()); print("complex_spectrogram_shape: {}, {}",
-    // complex_spectrogram.size(),
-    //     complex_spectrogram.at(0).size());
-
-    // spectrogram.get_local_maximums();
-    // auto local_max = spectrogram.get_local_maximums();
-    // auto spec = spectrogram.get_spectrogram();
-
-    // auto sppath = song.parent_path().parent_path() / "experiments" /
-    // (song.stem().string() + "_spec.csv"); auto ptspath =
-    // song.parent_path().parent_path() / "experiments" / (song.stem().string()
-    // + "_crits.csv"); print("Dumping spectrogram... @{}", sppath.string());
-    // csvWriteSpectrogram(spectrogram,sppath);
-    // csvWriteLocalMaxima(local_max,ptspath);
-    // db.insert(sp.get_features)
+  for (const auto &[song_name, count] : matches) {
+    std::cout << song_name << " : " << count << '\n';
   }
 
-  // db.search(ne_song)
-  */
+  spdlog::info("Total time to search for {} in songs_index: {} ms",
+
+               std::string{SONG_TO_SEARCH.stem()}, sw_total.elapsed().count());
 
   return 0;
 }
